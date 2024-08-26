9904891a283d2326aa641d2fcf3db5465d427679
yuudachi13 <t2pupa00@students.oamk.fi>
2024-08-27 01:28:22 +0300

    ehkä oikein

--- App.js
+++ App.js
@@ -1,20 +1,79 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import {Alert, Modal, StyleSheet, Text, Pressable, View, StatusBar,  } from 'react-native';
 
-export default function App() {
+const App = () => {
+  const [modalVisible, setModalVisible] = useState(false);
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.centeredView}>
+    <StatusBar backgroundColor="white" barStyle="dark-content" />
+
+      <Modal
+        animationType="fade"
+        transparent={false}
+        statusBarTranslucent ={false}
+        visible={modalVisible}
+        
+        onRequestClose={() => {
+         // Alert.alert('Modeli suljettu.'); // ei pyydetty mutta voi tehdä
+          setModalVisible(!modalVisible);
+        }}>
+        <View style={styles.centeredView}>
+        <StatusBar backgroundColor="gray" barStyle="dark-content" />
+          <View style={styles.modalView}>
+            <Text style={styles.modalText}>This is modal...</Text>
+            <Pressable
+              onPress={() => setModalVisible(!modalVisible)}>
+              <Text style={styles.textStyle}>Close</Text>
+            </Pressable>
+          </View>
+        </View>
+      </Modal>
+      <Pressable
+        onPress={() => setModalVisible(true)}>
+        <Text style={styles.textStyle1}>Show modal message</Text>
+      </Pressable>
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
-  container: {
+  centeredView: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    marginTop: 22,
+  },
+  modalView: {
+    backgroundColor: 'white',
+    padding: 35,
+    alignItems: 'center',
+    shadowOpacity: 0.50,
+    shadowRadius: 6,
+    elevation: 20,
+    top: 150, // näyttää olevan oikealla kohalla
+    width: '100%', 
+    position: 'absolute',
+
+    
+  },
+ 
+  textStyle: {
+    color: 'black',
+    textAlign: 'center',
+    fontWeight: 'bold',
+    fontSize: 16
+    
+  },
+  textStyle1: {
+    color: 'black',
+    textAlign: 'center',
+    fontSize: 20
+    
+  },
+  modalText: {
+    marginBottom: 30,
+    textAlign: 'center',
+    fontSize: 17
+
   },
 });
+export default App;
